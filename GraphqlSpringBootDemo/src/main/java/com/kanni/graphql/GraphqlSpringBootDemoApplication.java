f7984dfa6a5b3609aa0f4a1cb164418959decc21
Kanniselvan <selvankanni@gmail.com>
2022-11-13 16:01:24 +0530

    my first commit

---
+++ GraphqlSpringBootDemo/src/main/java/com/kanni/graphql/GraphqlSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kanni.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringBootDemoApplication.class, args);
+	}
+
+}
